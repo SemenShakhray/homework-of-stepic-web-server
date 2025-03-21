859bb33cd6af63fd1feb3f1795f37a87752cbb76
Shakhray <shakhrai.semion@yandex.ru>
2025-03-21 19:01:38 +0300

    added handler GetInfoInTable

--- 6-homework/db_explorer/db_explorer.go
+++ 6-homework/db_explorer/db_explorer.go
@@ -3,8 +3,8 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"net/http"
+	"strconv"
 )
 
 type Handler struct {
@@ -21,6 +21,7 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler.GetListTables)
+	mux.HandleFunc("/tables$", handler.GetInfoInTable)
 
 	return mux, nil
 
@@ -29,31 +30,54 @@ func NewDbExplorer(db *sql.DB) (http.Handler, error) {
 func (h *Handler) GetListTables(w http.ResponseWriter, r *http.Request) {
 	resp := h.store.GetListTables()
 
-	if resp.Error != nil {
-		if errors.Is(resp.Error, ErrUnknownTable) {
-			w.WriteHeader(http.StatusNotFound)
-			if err := json.NewEncoder(w).Encode(resp); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
+	CheckErrors(w, resp)
 
-			return
-		}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
-		if errors.Is(resp.Error, ErrRecordNotFound) {
-			w.WriteHeader(http.StatusNotFound)
-			if err := json.NewEncoder(w).Encode(resp); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
+func (h *Handler) GetInfoInTable(w http.ResponseWriter, r *http.Request) {
+	var limit, offset int
+	var err error
 
-			return
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+	tableName := r.FormValue("tableName")
+	if tableName == "" {
+		http.Error(w, ErrUnknownTable.Error(), http.StatusNotFound)
 
 		return
 	}
+
+	offsetString := r.FormValue("offset")
+	if offsetString == "" {
+		offset = 0
+	} else {
+		offset, err = strconv.Atoi(offsetString)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+
+			return
+		}
+
+	}
+
+	limitString := r.FormValue("limit")
+	if limitString == "" {
+		limit = 5
+	} else {
+		limit, err = strconv.Atoi(limitString)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+
+			return
+		}
+
+	}
+
+	resp := h.store.GetInfoInTable(limit, offset, tableName)
+
+	CheckErrors(w, resp)
 
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
--- 6-homework/db_explorer/errors.go
+++ 6-homework/db_explorer/errors.go
@@ -1,8 +1,41 @@
 package main
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
 
 var (
 	ErrUnknownTable   = errors.New("unknown table")
 	ErrRecordNotFound = errors.New("record not found")
 )
+
+func CheckErrors(w http.ResponseWriter, resp *Resp) {
+	if resp.Error != nil {
+		if errors.Is(resp.Error, ErrUnknownTable) {
+			w.WriteHeader(http.StatusNotFound)
+			if err := json.NewEncoder(w).Encode(resp); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			return
+		}
+
+		if errors.Is(resp.Error, ErrRecordNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			if err := json.NewEncoder(w).Encode(resp); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		return
+	}
+}
