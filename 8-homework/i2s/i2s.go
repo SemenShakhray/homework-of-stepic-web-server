a459a637c8c97635e9f864300bf765b6aff00d57
Shakhray <shakhrai.semion@yandex.ru>
2025-04-08 14:56:25 +0300

    second iteration

--- 8-homework/i2s/i2s.go
+++ 8-homework/i2s/i2s.go
@@ -1,63 +1,84 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"reflect"
 )
 
 func i2s(data interface{}, out interface{}) error {
-	inType := reflect.TypeOf(out)
-	if inType.Kind() != reflect.Ptr {
-		return fmt.Errorf("Type not pointer")
+	val := reflect.ValueOf(data)
+	res := reflect.ValueOf(out)
+	if res.Kind() != reflect.Ptr || res.IsNil() {
+		return fmt.Errorf("type not pointer or nil")
 	}
 
-	if dataMap, ok := data.(map[string]interface{}); ok {
-		result := make(map[string]interface{})
-		for key, value := range dataMap {
-			result[key] = value
-		}
-
-		res, err := json.Marshal(result)
-		if err != nil {
-			log.Println("failed marshaling map:", err)
-			return err
-		}
-		// log.Println(string(res))
-
-		err = json.Unmarshal(res, &out)
-		if err != nil {
-			log.Println("failed unmarshaling map:", err)
-			return err
-		}
+	if err := set(val, res.Elem()); err != nil {
+		return err
 	}
 
-	if dataSlice, ok := data.([]interface{}); ok {
-		result := make([]map[string]interface{}, 0)
+	return nil
+}
 
-		for _, item := range dataSlice {
-			if dataMap, ok := item.(map[string]interface{}); ok {
-				middleMap := make(map[string]interface{})
-				for key, value := range dataMap {
-					middleMap[key] = value
+func set(val, res reflect.Value) error {
+	if val.Kind() == reflect.Interface {
+		val = reflect.ValueOf(val.Interface())
+	}
+
+	log.Println("val:", val, "kind:", val.Kind(), "||", "res:", res, "kind:", res.Kind())
+
+	switch res.Kind() {
+
+	case reflect.Struct:
+		if val.Kind() != reflect.Map || val.Type().Key().Kind() != reflect.String {
+			return fmt.Errorf("wrong type value in")
+		}
+		for _, key := range val.MapKeys() {
+			field := res.FieldByName(key.String())
+			if !field.IsValid() {
+				continue
+			} else {
+				err := set(val.MapIndex(key), field)
+				if err != nil {
+					return err
 				}
-				result = append(result, middleMap)
 			}
+		}
 
-			res, err := json.Marshal(result)
+	case reflect.Slice:
+		if val.Kind() != reflect.Slice {
+			return fmt.Errorf("wrong type value in")
+		}
+		len := val.Len()
+		res.Set(reflect.MakeSlice(res.Type(), len, len))
+		for i := 0; i < len; i++ {
+			err := set(val.Index(i), res.Index(i))
 			if err != nil {
-				log.Println("failed marshaling slice:", err)
-				return err
-			}
-			// log.Println(string(res))
-
-			err = json.Unmarshal(res, &out)
-			if err != nil {
-				log.Println("failed unmarshaling slice:", err)
 				return err
 			}
 		}
+
+	case reflect.String:
+		if val.Kind() != reflect.String {
+			return fmt.Errorf("wrong type value in")
+		}
+		res.SetString(val.String())
+
+	case reflect.Int:
+		if val.Kind() != reflect.Float64 {
+			return fmt.Errorf("wrong type value in")
+		}
+		res.SetInt(int64(val.Float()))
+
+	case reflect.Bool:
+		if val.Kind() != reflect.Bool {
+			return fmt.Errorf("wrong type value in")
+		}
+		res.SetBool(val.Bool())
+
+	default:
+		return fmt.Errorf("unknow type")
 	}
+
 	return nil
 }
